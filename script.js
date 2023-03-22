9169b2eaaa1401a79974dff8c980d78c56ca3fc1
Abdullah Jaffrey <96979542+AbdullahJaffrey@users.noreply.github.com>
2023-03-22 18:35:43 +0500

    Assignment

---
+++ script.js
@@ -0,0 +1,5 @@
+
+var first_name = prompt('Kindly, Enter your first name: ');
+var last_name = prompt('Kindly, Enter your last name: ');
+alert('Hello '+ first_name + " " + last_name + '!'+ ' Welcome to our Site')
+alert('Your current time: ' +  Date())
