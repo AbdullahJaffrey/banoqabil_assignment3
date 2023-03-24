0460a92bc2d021f22743f9aa16f37ab2180c9183
Abdullah Jaffrey <96979542+AbdullahJaffrey@users.noreply.github.com>
2023-03-24 18:00:51 +0500

    Assignment

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var first_name = prompt('Kindly, Enter your first name: ');
 var last_name = prompt('Kindly, Enter your last name: ');
 alert('Hello '+ first_name + " " + last_name + '!'+ ' Welcome to our Site')
 
-var number1 = prompt('Kindly, Enter a number: ');
-var number2 = prompt('Kindly, Enter an another: ');
-alert('Addition: ', console.log(number1+number2));
-alert('Subtraction: ', console.log(number1-number2));
-alert('Multiplication: ', console.log(number1*number2));
-alert('Division: ', console.log(number1/number2));
+var number1 = Number(prompt('Kindly, Enter a number: '));
+var number2 = Number(prompt('Kindly, Enter an another: '));
+alert('Addition: ' +  (number1 + number2));
+alert('Subtraction: ' + (number1 - number2));
+alert('Multiplication: ' + number1*number2);
+alert('Division: ' + number1/number2);
 
 alert('Your current time: ' +  Date())
