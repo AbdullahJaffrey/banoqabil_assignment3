b93d4f7e5ad32b08c9c50ebd9eec11d3a0e0d583
Abdullah Jaffrey <96979542+AbdullahJaffrey@users.noreply.github.com>
2023-03-24 15:41:08 +0500

    Assignment

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var first_name = prompt('Kindly, Enter your first name: ');
 var last_name = prompt('Kindly, Enter your last name: ');
 alert('Hello '+ first_name + " " + last_name + '!'+ ' Welcome to our Site')
